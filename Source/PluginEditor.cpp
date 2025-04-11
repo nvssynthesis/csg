63dc6487254bf05aa433fa6d7b04d263de4f8901
Nicholas Solem <nsolem@ucsd.edu>
2025-04-10 18:52:21 -0700

    delete commented code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -35,180 +35,84 @@ CsgAudioProcessorEditor::CsgAudioProcessorEditor (CsgAudioProcessor& p)
     
     MemorySliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Memory", MemorySlider);
     MemorySlider.setRange((double)processor.memoryRange.start, (double)processor.memoryRange.end, 0.01f);
-//    MemorySlider.setSliderStyle(Slider::LinearBarVertical);
-//    MemorySlider.setNumDecimalPlacesToDisplay(3);
-//    MemorySlider.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, false, 104, 32);
     MemorySlider.setValue(1.f);
-//    addAndMakeVisible(&MemorySlider);
-//    MemorySlider.addListener(this);
-//    MemoryLabel.setFont(labelFont);
     MemoryLabel.setText("MEMORY", dontSendNotification);
-//    MemoryLabel.attachToComponent(&MemorySlider, false);
     
     FMsmoothSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "FM_Smooth", FMsmoothSlider);
     FMsmoothSlider.setRange((double)processor.smoothingRange.start, (double)processor.smoothingRange.end, 0.01f);
-//    FMsmoothSlider.setSliderStyle(Slider::LinearBarVertical);
     FMsmoothSlider.setSkewFactorFromMidPoint(2000.f);
-//    FMsmoothSlider.setNumDecimalPlacesToDisplay(3);
-//    FMsmoothSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, false, 104, 32);
     FMsmoothSlider.setValue(2000.f);
-//    addAndMakeVisible(&FMsmoothSlider);
-//    FMsmoothSlider.addListener(this);
-//    FMsmoothLabel.setFont(labelFont);
     FMsmoothLabel.setText("FM SMOOTH", dontSendNotification);
-//    FMsmoothLabel.attachToComponent(&FMsmoothSlider, false);
     
     Bits_A_SliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Bits_A", Bits_A_Slider);
     Bits_A_Slider.setRange((double)processor.BitsRange.start, (double)processor.BitsRange.end, 0.001f);
-//    Bits_A_Slider.setSliderStyle(Slider::LinearBarVertical);
-//    Bits_A_Slider.setNumDecimalPlacesToDisplay(3);
     Bits_A_Slider.setSkewFactorFromMidPoint(6.f);
-//    Bits_A_Slider.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, false, 104, 32);
     Bits_A_Slider.setValue(1024.f);
-//    addAndMakeVisible(&Bits_A_Slider);
-//    Bits_A_Slider.addListener(this);
-//    Bits_A_Label.setFont(labelFont);
     Bits_A_Label.setText("FM DEGRADE", dontSendNotification);
-//    Bits_A_Label.attachToComponent(&Bits_A_Slider, false);
     
     PM_preampSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "PM_Preamp", PM_preampSlider);
     PM_preampSlider.setRange((double)processor.PM_preamp_range.start, (double)processor.PM_preamp_range.end, 0.01f);
-//    PM_preampSlider.setSliderStyle(Slider::LinearBarVertical);
     PM_preampSlider.setSkewFactorFromMidPoint(2.f);
-//    PM_preampSlider.setNumDecimalPlacesToDisplay(3);
-//    PM_preampSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, false, 104, 32);
     PM_preampSlider.setValue(0.f);
-//    addAndMakeVisible(&PM_preampSlider);
-//    PM_preampSlider.addListener(this);
-//    PM_preampLabel.setFont(labelFont);
     PM_preampLabel.setText("PM AMOUNT", dontSendNotification);
-//    PM_preampLabel.attachToComponent(&PM_preampSlider, false);
     
     PMsmoothSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "PM_Smooth", PMsmoothSlider);
     PMsmoothSlider.setRange((double)processor.smoothingRange.start, (double)processor.smoothingRange.end, 0.01f);
-//    PMsmoothSlider.setSliderStyle(Slider::LinearBarVertical);
     PMsmoothSlider.setSkewFactorFromMidPoint(2000.f);
-//    PMsmoothSlider.setNumDecimalPlacesToDisplay(3);
-//    PMsmoothSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, false, 104, 32);
     PMsmoothSlider.setValue(2000.f);
-//    addAndMakeVisible(&PMsmoothSlider);
-//    PMsmoothSlider.addListener(this);
-//    PMsmoothLabel.setFont(labelFont);
     PMsmoothLabel.setText("PM TAME", dontSendNotification);
-//    PMsmoothLabel.attachToComponent(&PMsmoothSlider, false);
     
     Sin2CosSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Sin2Cos", Sin2CosSlider);
     Sin2CosSlider.setRange((double)processor.sin2cosRange.start, (double)processor.sin2cosRange.end, 0.01f);
-//    Sin2CosSlider.setSliderStyle(Slider::LinearBarVertical);
     Sin2CosSlider.setNumDecimalPlacesToDisplay(3);
-//    Sin2CosSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, false, 104, 32);
     Sin2CosSlider.setValue(0.0f);
-//    addAndMakeVisible(&Sin2CosSlider);
-//    Sin2CosSlider.addListener(this);
-//    Sin2CosLabel.setFont(labelFont);
     Sin2CosLabel.setText("PM SHAPE", dontSendNotification);
-//    Sin2CosLabel.attachToComponent(&Sin2CosSlider, false);
 
     Bits_B_SliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Bits_B", Bits_B_Slider);
     Bits_B_Slider.setRange((double)processor.BitsRange.start, (double)processor.BitsRange.end, 0.001f);
-//    Bits_B_Slider.setSliderStyle(Slider::LinearBarVertical);
-//    Bits_B_Slider.setNumDecimalPlacesToDisplay(3);
     Bits_B_Slider.setSkewFactorFromMidPoint(6.f);
-//    Bits_B_Slider.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, false, 104, 32);
     Bits_B_Slider.setValue(1024.f);
-//    addAndMakeVisible(&Bits_B_Slider);
-//    Bits_B_Slider.addListener(this);
-//    Bits_B_Label.setFont(labelFont);
     Bits_B_Label.setText("PM DEGRADE", dontSendNotification);
-//    Bits_B_Label.attachToComponent(&Bits_B_Slider, false);
+
     
     DroneSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Drone", DroneSlider);
     DroneSlider.setRange((double)processor.droneRange.start, (double)processor.droneRange.end, 0.01f);
-//    DroneSlider.setSliderStyle(Slider::LinearBarVertical);
-//    DroneSlider.setNumDecimalPlacesToDisplay(3);
     DroneSlider.setSkewFactorFromMidPoint(0.707f);
-//    DroneSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, false, 104, 32);
     DroneSlider.setValue(0.f);
-//    addAndMakeVisible(&DroneSlider);
-//    DroneSlider.addListener(this);
-//    DroneLabel.setFont(labelFont);
     DroneLabel.setText("DRONE", dontSendNotification);
-//    DroneLabel.attachToComponent(&DroneSlider, false);
     
     RiseSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Rise", RiseSlider);
     RiseSlider.setRange((double)processor.slewRange.start, (double)processor.slewRange.end, 0.01f);
-//    RiseSlider.setSliderStyle(Slider::LinearBarVertical);
-//    RiseSlider.setNumDecimalPlacesToDisplay(3);
     RiseSlider.setSkewFactorFromMidPoint(100.f);
-//    RiseSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, false, 104, 32);
     RiseSlider.setValue(1.f);
-//    addAndMakeVisible(&RiseSlider);
-//    RiseSlider.addListener(this);
-//    RiseLabel.setFont(labelFont);
     RiseLabel.setText("RISE", dontSendNotification);
-//    RiseLabel.attachToComponent(&RiseSlider, false);
     
     FallSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Fall", FallSlider);
     FallSlider.setRange((double)processor.slewRange.start, (double)processor.slewRange.end, 0.01f);
-//    FallSlider.setSliderStyle(Slider::LinearBarVertical);
-//    FallSlider.setNumDecimalPlacesToDisplay(3);
     FallSlider.setSkewFactorFromMidPoint(100.f);
-//    FallSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, false, 104, 32);
     FallSlider.setValue(1.f);
-//    addAndMakeVisible(&FallSlider);
-//    FallSlider.addListener(this);
-//    FallLabel.setFont(labelFont);
     FallLabel.setText("FALL", dontSendNotification);
-//    FallLabel.attachToComponent(&FallSlider, false);
     
     CutoffSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Cutoff", CutoffSlider);
     CutoffSlider.setRange((double)processor.cutoffRange.start, (double)processor.cutoffRange.end, 0.001f);
-//    CutoffSlider.setSliderStyle(Slider::LinearBarVertical);
-//    CutoffSlider.setNumDecimalPlacesToDisplay(3);
     CutoffSlider.setSkewFactorFromMidPoint(1000.f);
-//    CutoffSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, false, 104, 32);
     CutoffSlider.setValue(16000.f);
-//    addAndMakeVisible(&CutoffSlider);
-//    CutoffSlider.addListener(this);
-//    CutoffLabel.setFont(labelFont);
     CutoffLabel.setText("CUTOFF", dontSendNotification);
-//    CutoffLabel.attachToComponent(&CutoffSlider, false);
     
     ResSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Resonance", ResSlider);
     ResSlider.setRange((double)processor.resRange.start, (double)processor.resRange.end, 0.001f);
-//    ResSlider.setSliderStyle(Slider::LinearBarVertical);
-//    ResSlider.setNumDecimalPlacesToDisplay(3);
-//    ResSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, false, 104, 32);
     ResSlider.setValue(1.f);
-//    addAndMakeVisible(&ResSlider);
-//    ResSlider.addListener(this);
-//    ResLabel.setFont(labelFont);
     ResLabel.setText("RESON", dontSendNotification);
-//    ResLabel.attachToComponent(&ResSlider, false);
     
     LFOspeedSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "LFO_Speed", LFOspeedSlider);
     LFOspeedSlider.setRange((double)processor.lfoSpeedRange.start, (double)processor.lfoSpeedRange.end, 0.001f);
-//    LFOspeedSlider.setSliderStyle(Slider::LinearBarVertical);
-//    LFOspeedSlider.setNumDecimalPlacesToDisplay(3);
-//    LFOspeedSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, false, 104, 32);
     LFOspeedSlider.setValue(1.f);
-//    addAndMakeVisible(&LFOspeedSlider);
-//    LFOspeedSlider.addListener(this);
-//    LFOspeedLabel.setFont(labelFont);
     LFOspeedLabel.setText("LFO RATE", dontSendNotification);
-//    LFOspeedLabel.attachToComponent(&LFOspeedSlider, false);
     
     LFOwaveSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "LFO_Wave", LFOwaveSlider);
     LFOwaveSlider.setRange((double)processor.lfoWaveRange.start, (double)processor.lfoWaveRange.end, 0.001f);
-//    LFOwaveSlider.setSliderStyle(Slider::LinearBarVertical);
-//    LFOwaveSlider.setNumDecimalPlacesToDisplay(3);
-//    LFOwaveSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, false, 104, 32);
     LFOwaveSlider.setValue(0.f);
-//    addAndMakeVisible(&LFOwaveSlider);
-//    LFOwaveSlider.addListener(this);
-//    LFOwaveLabel.setFont(labelFont);
     LFOwaveLabel.setText("LFO WAVE", dontSendNotification);
-//    LFOwaveLabel.attachToComponent(&LFOwaveSlider, false);
     for (int n=0; n<N_PARAMS_MOD;n++){
         (*sliders_mod[n]).setSliderStyle(Slider::Rotary);
         (*sliders_mod[n]).setTextBoxStyle(Slider::NoTextBox, false, 10, 10);
@@ -218,75 +122,30 @@ CsgAudioProcessorEditor::CsgAudioProcessorEditor (CsgAudioProcessor& p)
     }
     MODselfFM_SliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Self_FM_MOD", MODselfFM_Slider);
     MODselfFM_Slider.setRange(0.f, 1.f, 0.01f);    // will just be scaled by processor
-//    MODselfFM_Slider.setSliderStyle(Slider::Rotary);
-//    MODselfFM_Slider.setTextBoxStyle(Slider::NoTextBox, false, 10, 10);
-//    MODselfFM_Slider.setValue(0.f);
-//    addAndMakeVisible(&MODselfFM_Slider);
-//    MODselfFM_Slider.addListener(this);
     
     MODmemorySliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Memory_MOD", MODmemorySlider);
     MODmemorySlider.setRange(0.f, 1.f, 0.01f);    // will just be scaled by processor
-//    MODmemorySlider.setSliderStyle(Slider::Rotary);
-//    MODmemorySlider.setTextBoxStyle(Slider::NoTextBox, false, 10, 10);
-//    MODmemorySlider.setValue(0.f);
-//    addAndMakeVisible(&MODmemorySlider);
-//    MODmemorySlider.addListener(this);
     
     MODFMsmoothSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "FM_Smooth_MOD", MODFMsmoothSlider);
     MODFMsmoothSlider.setRange(0.f, 1.f, 0.01f);    // will just be scaled by processor
-//    MODFMsmoothSlider.setSliderStyle(Slider::Rotary);
-//    MODFMsmoothSlider.setTextBoxStyle(Slider::NoTextBox, false, 10, 10);
-//    MODFMsmoothSlider.setValue(0.f);
-//    addAndMakeVisible(&MODFMsmoothSlider);
-//    MODFMsmoothSlider.addListener(this);
     
     MODBits_A_SliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Bits_A_MOD", MODBits_A_Slider);
     MODBits_A_Slider.setRange(0.f, 1.f, 0.01f);    // will just be scaled by processor
-//    MODBits_A_Slider.setSliderStyle(Slider::Rotary);
-//    MODBits_A_Slider.setTextBoxStyle(Slider::NoTextBox, false, 10, 10);
-//    MODBits_A_Slider.setValue(0.f);
-//    addAndMakeVisible(&MODBits_A_Slider);
-//    MODBits_A_Slider.addListener(this);
     
     MODPM_preampSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "PM_Preamp_MOD", MODPM_preampSlider);
     MODPM_preampSlider.setRange(0.f, 1.f, 0.01f);    // will just be scaled by processor
-//    MODPM_preampSlider.setSliderStyle(Slider::Rotary);
-//    MODPM_preampSlider.setTextBoxStyle(Slider::NoTextBox, false, 10, 10);
-//    MODPM_preampSlider.setValue(0.f);
-//    addAndMakeVisible(&MODPM_preampSlider);
-//    MODPM_preampSlider.addListener(this);
     
     MODPMsmoothSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "PM_Smooth_MOD", MODPMsmoothSlider);
     MODPMsmoothSlider.setRange(0.f, 1.f, 0.01f);    // will just be scaled by processor
-//    MODPMsmoothSlider.setSliderStyle(Slider::Rotary);
-//    MODPMsmoothSlider.setTextBoxStyle(Slider::NoTextBox, false, 10, 10);
-//    MODPMsmoothSlider.setValue(0.f);
-//    addAndMakeVisible(&MODPMsmoothSlider);
-//    MODPMsmoothSlider.addListener(this);
     
     MODSin2CosSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Sin2Cos_MOD", MODSin2CosSlider);
     MODSin2CosSlider.setRange(0.f, 1.f, 0.01f);    // will just be scaled by processor
-//    MODSin2CosSlider.setSliderStyle(Slider::Rotary);
-//    MODSin2CosSlider.setTextBoxStyle(Slider::NoTextBox, false, 10, 10);
-//    MODSin2CosSlider.setValue(0.f);
-//    addAndMakeVisible(&MODSin2CosSlider);
-//    MODSin2CosSlider.addListener(this);
     
     MODBits_B_SliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Bits_B_MOD", MODBits_B_Slider);
     MODBits_B_Slider.setRange(0.f, 1.f, 0.01f);    // will just be scaled by processor
-//    MODBits_B_Slider.setSliderStyle(Slider::Rotary);
-//    MODBits_B_Slider.setTextBoxStyle(Slider::NoTextBox, false, 10, 10);
-//    MODBits_B_Slider.setValue(0.f);
-//    addAndMakeVisible(&MODBits_B_Slider);
-//    MODBits_B_Slider.addListener(this);
     
     MODCutoffSliderAttachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Cutoff_MOD", MODCutoffSlider);
     MODCutoffSlider.setRange(0.f, 1.f, 0.01f);    // will just be scaled by processor
-//    MODCutoffSlider.setSliderStyle(Slider::Rotary);
-//    MODCutoffSlider.setTextBoxStyle(Slider::NoTextBox, false, 10, 10);
-//    MODCutoffSlider.setValue(0.f);
-//    addAndMakeVisible(&MODCutoffSlider);
-//    MODCutoffSlider.addListener(this);
     
     filterTypeSlider_L_Attachment = new AudioProcessorValueTreeState::SliderAttachment (processor.parameters, "Filter Type Left", filterTypeSlider_L);
     filterTypeSlider_L.setRange(0.f, 3.f, 1.f);
